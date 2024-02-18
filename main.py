1ba460e033daa25c8738300d76111c54c910e274
murali <muralimano86@gmail.com>
2024-02-18 15:12:36 +0000

    Create a whitened image of delta, find contours and put a rectangle in contours, and add it to Original frame

--- main.py
+++ main.py
@@ -15,8 +15,18 @@ while True:
         first_frame = grey_frame_gau
 
     delta_frame = cv2.absdiff(first_frame, grey_frame_gau)
+    thresh_frame = cv2.threshold(delta_frame, 50, 255, cv2.THRESH_BINARY)[1]
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
-    cv2.imshow("My video", delta_frame)
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        if cv2.contourArea(contour) < 10000:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+    cv2.imshow("Video", frame)
 
     key = cv2.waitKey(1)
 
