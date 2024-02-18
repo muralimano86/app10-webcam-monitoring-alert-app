8ed63be5faad91ecf30532fc9847820b5deec51c
murali <muralimano86@gmail.com>
2024-02-18 14:05:40 +0000

    Capture the difference in frames as black and white delta frame

--- main.py
+++ main.py
@@ -4,9 +4,19 @@ import time
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
+first_frame = None
+
 while True:
     check, frame = video.read()
-    cv2.imshow("My video", frame)
+    grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    grey_frame_gau = cv2.GaussianBlur(grey_frame, (21, 21), 0)
+
+    if first_frame is None:
+        first_frame = grey_frame_gau
+
+    delta_frame = cv2.absdiff(first_frame, grey_frame_gau)
+
+    cv2.imshow("My video", delta_frame)
 
     key = cv2.waitKey(1)
 
