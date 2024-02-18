a2fa6b5c1427bed32341f6e45d8548166fb93020
murali <muralimano86@gmail.com>
2024-02-18 15:39:56 +0000

    Call email function when new object left the frame

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import cv2
 import time
+from backend import send_email
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
+status_list = []
 
 while True:
+    status = 0
     check, frame = video.read()
     grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     grey_frame_gau = cv2.GaussianBlur(grey_frame, (21, 21), 0)
@@ -24,7 +27,15 @@ while True:
         if cv2.contourArea(contour) < 10000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        if rectangle.any():
+            status = 1
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email()
 
     cv2.imshow("Video", frame)
 
